e23b46064585589acd4a4b25e9c1e097ca24a5f5
Leo Araya <larayavrs@gmail.com>
2025-11-08 20:29:48 -0300

    feat: implement Natives class for INI file handling with open and close methods

---
+++ source/natives.cpp
@@ -0,0 +1,71 @@
+#include <map>
+#include <string>
+#include <cstring>
+
+#include "handler.hpp"
+#include "natives.hpp"
+#include "constants.hpp"
+
+// so we storage the the INI file handles
+std::map<int, Handler *> handlers; /** A map of file handles to Handler objects. */
+int next_handle = 1;               /** The next available handle, incremented for each new handler. */
+
+std::string GetStringFromAMX(AMX *amx, cell param)
+{
+    cell *addr = NULL;
+    int len = 0;
+    amx_GetAddr(amx, param, &addr);
+    amx_StrLen(addr, &len);
+    if (len > 0)
+    {
+        char *str = new char[len + 1];
+        amx_GetString(str, addr, 0, len + 1);
+        std::string result(str);
+        delete[] str;
+        return result;
+    }
+    return "";
+}
+
+void SetStringToAMX(AMX *amx, cell param, const std::string &str, int maxlen)
+{
+    cell *addr = NULL;
+    amx_GetAddr(amx, param, &addr);
+    amx_SetString(addr, str.c_str(), 0, 0, maxlen);
+}
+
+cell AMX_NATIVE_CALL Natives::Native_INI_Open(AMX *amx, cell *params)
+{
+    std::string path = GetStringFromAMX(amx, params[1]);
+    if (path.empty())
+    {
+        logprintf("[pawn-ini | Error] Empty path provided for INI_Open");
+        return 0;
+    }
+    Handler *handler = new Handler(path);
+    if (!handler->is_valid())
+    {
+        logprintf("[path-ini | Error] Failed to open INI file at %s", path.c_str());
+        delete handler;
+        return 0;
+    }
+    int handle = next_handle++;
+    handlers[handle] = handler;
+    logprintf("[pawn-ini | Info] Opened INI file at %s with handle %d", path.c_str(), handle);
+    return handle;
+}
+
+cell AMX_NATIVE_CALL Natives::Native_INI_Close(AMX *amx, cell *params)
+{
+    int handle = params[1];
+    // TODO: check if handle is valid?
+    auto it = handlers.find(handle);
+    if (it == handlers.end())
+    {
+        logprintf("[pawn-ini | Error] Invalid handle %d provided for INI_Close", handle);
+        return 0;
+    }
+    delete it->second;
+    handlers.erase(it);
+    return 1;
+}
