f18257ddf1acf83c21a48d93716581bbc4bed23c
Leo Araya <larayavrs@gmail.com>
2025-11-21 16:44:04 -0300

    feat: now pawn-ini supported for 32-bit servers

--- compile.bat
+++ compile.bat
@@ -1,4 +1,5 @@
 @echo off
+set SCRIPT_DIR=%~dp0.
 echo ========================================
 echo   pawn-ini - compilation script
 echo   Version: v0.1.0
@@ -6,12 +7,18 @@ echo   Platform: Windows
 echo ========================================
 
 if /I "%1"=="clean" (
-    echo [*] Cleaning build directory...
+    echo [*] Cleaning build and output directories...
     if exist build (
         rmdir /s /q build
         echo Build directory removed
     ) else (
         echo Build directory does not exist
+    )
+    if exist output (
+        rmdir /s /q output
+        echo Output directory removed
+    ) else (
+        echo Output directory does not exist
     )
     echo.
 )
@@ -32,61 +39,111 @@ if not exist build (
     mkdir build
 )
 
-cd build
+echo.
+echo ========================================
+echo   Building 32-bit (Win32)
+echo ========================================
+call :build_arch Win32 x32
+if %errorlevel% neq 0 goto :build_failed
 
-echo * Configuring project with CMake...
-cmake .. -G "Visual Studio 17 2022"
+echo.
+echo ========================================
+echo   Building 64-bit (x64)
+echo ========================================
+call :build_arch x64 x64
+if %errorlevel% neq 0 goto :build_failed
+
+echo.
+echo All builds successful!
+
+REM Copy final artifacts to repo root (32-bit plugin + include)
+if exist output\x32\pawn-ini.dll copy /Y output\x32\pawn-ini.dll pawn-ini.dll >nul
+if exist output\pawn-ini.inc copy /Y output\pawn-ini.inc pawn-ini.inc >nul
+
+echo Compilation has been completed
+echo Compiled files in output/:
+dir /b output
+echo.
+echo Root artifacts:
+if exist pawn-ini.dll echo   pawn-ini.dll (32-bit)
+if exist pawn-ini.inc echo   pawn-ini.inc
+echo.
+echo Plugin size (32-bit in root):
+for %%F in (pawn-ini.dll) do if exist %%F echo   pawn-ini.dll: %%~zF bytes
+echo.
+echo Files are ready in the repo root (pawn-ini.dll / pawn-ini.inc) and in 'output/x32' and 'output/x64'.
+echo.
+echo Installation (typical SA-MP 32-bit server):
+echo   1. Copy pawn-ini.dll from output\x32 or repo root to your server's plugins folder
+echo   2. Copy pawn-ini.inc to your server's pawno\include folder
+echo   3. Add 'plugins pawn-ini' to server.cfg
+echo   4. Include ^<pawn-ini^> in your gamemode
+echo.
+pause
+exit /b 0
+
+:build_arch
+REM %1 = CMake -A value (Win32/x64)
+REM %2 = architecture label (x32/x64)
+setlocal ENABLEDELAYEDEXPANSION
+set ARCH_GENERATOR=%1
+set ARCH_LABEL=%2
+set BUILD_SUBDIR=build\%ARCH_LABEL%
+
+echo * Using build directory: !BUILD_SUBDIR!
+if not exist "!BUILD_SUBDIR!" (
+    mkdir "!BUILD_SUBDIR!"
+)
+
+pushd "!BUILD_SUBDIR!"
+
+echo * Configuring project with CMake for !ARCH_LABEL!...
+cmake "%SCRIPT_DIR%" -G "Visual Studio 17 2022" -A !ARCH_GENERATOR! -DPAWN_INI_ARCH=!ARCH_LABEL!
 
 if %errorlevel% neq 0 (
     echo.
-    echo ! ERROR: CMake configuration failed
+    echo ! ERROR: CMake configuration failed for !ARCH_LABEL! with VS 2022
     echo ! Trying with Visual Studio 16 2019...
     echo.
-    cmake .. -G "Visual Studio 16 2019"
+    cmake "%SCRIPT_DIR%" -G "Visual Studio 16 2019" -A !ARCH_GENERATOR! -DPAWN_INI_ARCH=!ARCH_LABEL!
     
     if %errorlevel% neq 0 (
         echo.
-        echo ! ERROR: CMake configuration failed
-        echo ! Please check that Visual Studio is installed
-        cd ..
-        pause
-        exit /b 1
+        echo ! ERROR: CMake configuration failed for !ARCH_LABEL!
+        echo ! Please check that the corresponding Visual Studio toolset is installed
+        popd
+        endlocal & exit /b 1
     )
 )
 
-echo * Configuration successful
-echo * Building project in Release mode...
+echo * Configuration successful for !ARCH_LABEL!
+echo * Building project in Release mode for !ARCH_LABEL!...
 echo.
 cmake --build . --config Release
 
 if %errorlevel% neq 0 (
     echo.
-    echo ! ERROR: Build failed
-    cd ..
-    pause
-    exit /b 1
+    echo ! ERROR: Build failed for !ARCH_LABEL!
+    popd
+    endlocal & exit /b 1
 )
 
-echo Build successful!
-
-echo Installing files to output directory...
+echo * Installing files to output directory for !ARCH_LABEL!...
 cmake --install . --config Release
 
-cd ..
+if %errorlevel% neq 0 (
+    echo.
+    echo ! ERROR: Install failed for !ARCH_LABEL!
+    popd
+    endlocal & exit /b 1
+)
 
-echo Compilation has been complete
-echo Compiled files:
-dir /b output
+popd
+endlocal & exit /b 0
+
+:build_failed
 echo.
-echo Plugin size:
-for %%F in (output\pawn-ini.dll) do echo   pawn-ini.dll: %%~zF bytes
-echo.
-echo Files are ready in the 'output' directory
-echo.
-echo Installation:
-echo   1. Copy output\pawn-ini.dll to your server's plugins folder
-echo   2. Copy output\pawn-ini.inc to your server's pawno\include folder
-echo   3. Add 'plugins pawn-ini' to server.cfg
-echo   4. Include ^<pawn-ini^> in your gamemode
+echo ! ERROR: One or more builds failed. See messages above.
 echo.
 pause
+exit /b 1
